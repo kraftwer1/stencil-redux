c69431b280656460ddc34c3d3e392704491b116b
Kühne Alex, SAS-OXD-MYA <Alex.Kuehne@swisscom.com>
2019-01-27 20:00:00 +0100

    Dist files

---
+++ dist/types/global/interfaces.d.ts
@@ -0,0 +1,10 @@
+export interface Store {
+    dispatch: () => any;
+    subscribe: (cb: Function) => any;
+    getState: () => any;
+    getStore: () => any;
+    setStore: (any: any) => void;
+    mapStateToProps: (component: any, props: any) => () => void;
+    mapDispatchToProps: (component: any, props: any) => void;
+}
+export declare type Action = Function;
